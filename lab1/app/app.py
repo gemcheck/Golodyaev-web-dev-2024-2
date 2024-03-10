aa1ac75e0202785de94416d479f67f71749be965
Golodyaev Maxim <golodyaev.maksim@wildberries.work>
2024-03-10 11:03:58 +0300

    lab1

--- lab1/app/app.py
+++ lab1/app/app.py
@@ -1,8 +1,59 @@
-from flask import Flask
+import random
+from flask import Flask, render_template
+from faker import Faker
+
+fake = Faker()
 
 app = Flask(__name__)
 application = app
 
+images_ids = ['1',
+              '2',
+              '3',
+              '4',
+              '5']
+
+
+def generate_comments(replies=True):
+    comments = []
+    for i in range(random.randint(1, 3)):
+        comment = {'author': fake.name(), 'text': fake.text()}
+        if replies:
+            comment['replies'] = generate_comments(replies=False)
+        comments.append(comment)
+    return comments
+
+
+def generate_post(i):
+    return {
+        'title': fake.city(),
+        'text': fake.paragraph(nb_sentences=100),
+        'author': fake.name(),
+        'date': fake.date_between(start_date='-2y', end_date='now'),
+        'image_id': f'{images_ids[i]}.jpg',
+        'comments': generate_comments()
+    }
+
+
+posts_list = sorted([generate_post(i) for i in range(5)], key=lambda p: p['date'], reverse=True)
+
+
 @app.route('/')
 def index():
-    return 'Hello!'
+    return render_template('index.html')
+
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html', title='Посты', posts=posts_list)
+
+
+@app.route('/posts/<int:index>')
+def post(index):
+    p = posts_list[index]
+    return render_template('post.html', title=p['title'], post=p)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='Об авторе')
